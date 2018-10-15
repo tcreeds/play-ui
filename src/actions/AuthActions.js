a39d2b660d95ad8844c70d04d42ea4eaf4535a80
Trevor Creed <tcreeds@gmail.com>
2018-10-15 14:17:49 -0400

    Update api url logic

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -2,11 +2,11 @@
 import Axios from 'axios'
 import router from '@/router'
 import {
-    LOGIN_URL, BASE_URL
+    LOGIN_URL, BASE_URL_LOCAL, BASE_URL_DEPLOYED
 } from '@/constants/api.js' 
 import { LOGIN_SUCCESSFUL, LOGOUT } from '@/constants/AuthActionTypes.js'
 
-
+const BASE_URL = window.location.host.indexOf("localhost") == 0 ? BASE_URL_LOCAL : BASE_URL_DEPLOYED
 
 export function login(commit, credentials){
     return Axios.post(`${BASE_URL}${LOGIN_URL}`, credentials)
--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,4 @@
-export const BASE_URL = 'http://localhost:8080/'
+export const BASE_URL_LOCAL = 'http://localhost:8080/'
+export const BASE_URL_DEPLOYED = 'https://play-api.tcreeds.io/'
 
 export const LOGIN_URL = 'users/login'
